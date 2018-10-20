20667fa0d2674318f3487425cdb6c9b1840f9251
4DR14NV4LD3Z <adrian.valdezvega@hotmail.com>
2018-10-20 18:23:19 -0500

    1.0.0

---
+++ buroCredito.php
@@ -0,0 +1,20 @@
+<?php
+	// Convertimos la matriz a una cadena con formato XML.
+	$xml = '<?xml version="1.0" encoding="UTF-8"?>
+<tns:creditoRequest xsi:schemaLocation="http://itq.edu/credito credito.xsd" xmlns:tns="http://itq.edu/credito" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+	<tns:nombre>'.htmlspecialchars($_POST['firstname']).'</tns:nombre>
+	<tns:apellidos>'.htmlspecialchars($_POST['lastname']).'</tns:apellidos>
+	<tns:curp>'.htmlspecialchars($_POST['curptype']).'</tns:curp>
+	<tns:domicilio>'.htmlspecialchars($_POST['address']).'</tns:domicilio>
+	<tns:salario>'.(int)$_POST['salary'].'</tns:salario>
+	<tns:monto>'.(int)$_POST['amount'].'</tns:monto>
+	<tns:tasaInteres>'.(int)$_POST['interestRate'].'</tns:tasaInteres>
+	<tns:plazo>'.(int)$_POST['term'].'</tns:plazo>
+</tns:creditoRequest>';
+	// Grabamos el XML en el servidor como un fichero plano, para
+	// poder ser leido por otra aplicación.
+	$nombrearchivo = "infcliente.xml";
+	$gestor = fopen($nombrearchivo, 'w+');
+	fwrite($gestor, $xml);
+	fclose($gestor);
+?>
